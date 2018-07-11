9c22cfa0bbf1a09ff5c8b94e0406dd5ddc21c924
Bella Buchanan <isabellabuchanan1@gmail.com>
2018-07-11 12:06:21 +0900

    new quiz 1

--- quiz1.R
+++ quiz1.R
@@ -1,1 +1,47 @@
-hello
+#1
+
+install.packages("Matching")
+
+library(Matching)
+
+#2
+
+data(lalonde)
+
+#3
+
+str(lalonde)
+
+#there are 445 observations of 12 variables
+
+#4
+
+colnames(lalonde)
+
+#colnames are "age"     "educ"    "black"   "hisp"    "married"
+# "nodegr"  "re74"    "re75"    "re78"    "u74"    
+# "u75"     "treat" 
+
+#5
+
+# 12 variables, found this out in question 1
+
+#6
+
+max(lalonde["re74"])
+#max is 39570.7
+
+#7
+
+min(lalonde["re74"])
+
+# min is zero
+
+sum(lalonde["re74"] == 0)
+
+
+#8
+
+#9
+#
+#
